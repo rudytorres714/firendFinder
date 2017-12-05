eaa79e1f5bdfdb2bb921a241be6bd7f0b1c693ab
RUDY TORRES <rudy.torres714@gmail.com>
2017-12-05 10:51:01 -0800

    my files upload

---
+++ data/tableData.js
@@ -0,0 +1,187 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserved tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var tableArray = [
+    {
+        name: "Ahmed",
+        img: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: "Jacob Deming",
+        img: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+        scores: [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        name: "Jeremiah Scanlon",
+        img: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+        scores: [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name: "Louis T. Delia",
+        img: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        scores: [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        name: "Lou Ritter",
+        img: "https://m.facebook.com/img.php?fbid=10208500699025296&id=1542229019&set=a.1549418665704.77596.1542229019&source=11",
+        scores: [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        name: "Jordan Biason",
+        img: "https://scontent-ord1-1.xx.fbcdn.net/v/t1.0-9/12741971_10205764267089153_4212986785721953092_n.jpg?oh=4e18265f7d380167223a97fbd7eba278&oe=57B78445",
+        scores: [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    },
+    {
+        name: "hunk",
+        img: "https://img.buzzfeed.com/store-an-image-prod-us-east-1/Skiy3MyD-.png?output-format=jpg?output-format=jpg&downsize=660:*&output-quality=auto",
+        scores: [
+            "3",
+            "4",
+            "2",
+            "1",
+            "4",
+            "1",
+            "2",
+            "1",
+            "4",
+            "2"
+        ]
+    },
+    {
+        name: "chris",
+        img: "https://code.jquery.com/jquery.js",
+        scores: [
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2"
+        ]
+    },
+    {
+        name: "chris",
+        img: "https://code.jquery.com/jquery.js",
+        scores: [
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2"
+        ]
+    },
+    {
+        name: "chris",
+        img: "/api/friends",
+        scores: [
+            "2",
+            "1",
+            "1",
+            "1",
+            "2",
+            "3",
+            "2",
+            "3",
+            "2",
+            "2"
+        ]
+    },
+    {
+        name: "Katrina",
+        img: "//html",
+        scores: [
+            "1",
+            "2",
+            "3",
+            "2",
+            "4",
+            "2",
+            "3",
+            "5",
+            "3",
+            "3"
+        ]
+    }
+];
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = tableArray;
